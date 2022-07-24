25ecff6cb453300eb2d368e7470a4f63d3a822be
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-07-24 14:25:08 +0200

    reorganize test variables

--- test/test_mysprite.py
+++ test/test_mysprite.py
@@ -12,57 +12,55 @@ class TestMySprite(unittest.TestCase):
         pg.display.set_caption("Sprite Test")
         self.clock = pg.time.Clock()
 
-        self.mysprite = MySprite((400, 300), (0, 0))
-
-        self.group = pg.sprite.Group()
-        self.group.add(self.mysprite)
-
     def test_blank_sprite(self):
-        """Click the sprite to signal the successful completion of the test."""
+        """Close the window to end the test."""
+        mysprite = MySprite((400, 300), (0, 0))
+        group = pg.sprite.Group()
+        group.add(mysprite)
+
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    return True
-                if event.type == pg.MOUSEBUTTONUP and self.mysprite.rect.collidepoint(event.pos):
-                    return True
+                    return
 
             self.screen.fill(pg.Color("green"))
 
             # test logic here
-            self.mysprite.rect.center = self.mysprite.pos
-            self.group.draw(self.screen)
+            mysprite.rect.center = mysprite.pos
 
-            self.group.update()
+            group.draw(self.screen)
+            group.update()
 
             pg.display.flip()
             self.clock.tick(60)
 
     def test_bounce_sprite(self):
-        """Click the sprite to signal the successful completion of the test."""
-        self.mysprite.speed = (1, 3)
+        """Close the window to end the test."""
+        mysprite = MySprite((400, 300), (1, 3))
+        group = pg.sprite.Group()
+        group.add(mysprite)
+
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    return False
-                if event.type == pg.MOUSEBUTTONUP and self.mysprite.rect.collidepoint(event.pos):
-                    return True
+                    return
 
             self.screen.fill(pg.Color("green"))
 
             # test logic here
-            if not self.screen_rect.contains(self.mysprite.rect):
-                if self.screen_rect.left > self.mysprite.rect.left:
-                    self.mysprite.speed.reflect_ip((1, 0))
-                if self.screen_rect.right < self.mysprite.rect.right:
-                    self.mysprite.speed.reflect_ip((-1, 0))
-                if self.screen_rect.top > self.mysprite.rect.top:
-                    self.mysprite.speed.reflect_ip((0, 1))
-                if self.screen_rect.bottom < self.mysprite.rect.bottom:
-                    self.mysprite.speed.reflect_ip((0, -1))
+            if not self.screen_rect.contains(mysprite.rect):
+                if self.screen_rect.left > mysprite.rect.left:
+                    mysprite.speed.reflect_ip((1, 0))
+                if self.screen_rect.right < mysprite.rect.right:
+                    mysprite.speed.reflect_ip((-1, 0))
+                if self.screen_rect.top > mysprite.rect.top:
+                    mysprite.speed.reflect_ip((0, 1))
+                if self.screen_rect.bottom < mysprite.rect.bottom:
+                    mysprite.speed.reflect_ip((0, -1))
 
-            self.group.update()
-            self.mysprite.rect.center = self.mysprite.pos
-            self.group.draw(self.screen)
+            group.update()
+            mysprite.rect.center = mysprite.pos
+            group.draw(self.screen)
 
             pg.display.flip()
             self.clock.tick(60)
