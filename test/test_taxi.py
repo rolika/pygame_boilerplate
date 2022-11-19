12d5a326f451b072ca469ef153c4c6a48520d3eb
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-11-19 14:15:30 +0100

    taxi bounces off the cave walls

--- test/test_taxi.py
+++ test/test_taxi.py
@@ -11,9 +11,6 @@ FRICTION = (0.95, 0.95)
 GRAVITY = (0, 0.00005)
 
 
-
-
-
 class TestTaxi(unittest.TestCase):
     """Close the window to end the tests."""
 
@@ -23,11 +20,10 @@ class TestTaxi(unittest.TestCase):
         self.screen_rect = self.screen.get_rect()
         pg.display.set_caption("Sprite Test")
         self.clock = pg.time.Clock()
-    
 
     def test_taxi_game(self):
-        scene_bgr = pg.image.load("test/taxi_scene.png")
-        scene_mask = pg.mask.from_surface(scene_bgr)
+        level = MySprite((0, 0), (0, 0), "test/taxi_scene.png")
+        scene = pg.sprite.GroupSingle(level)
 
         taxi = MySprite((300, 200), (0, 0), size=(16, 8), color="orange", gravity=GRAVITY, friction=FRICTION)
         player = pg.sprite.GroupSingle(taxi)
@@ -36,10 +32,10 @@ class TestTaxi(unittest.TestCase):
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     return
-            
+
             self.screen.fill("black")
-            self.screen.blit(scene_bgr, (0, 0))
-            
+            scene.draw(self.screen)
+
             # handle key input
             keys = pg.key.get_pressed()
             if keys[pg.K_KP4]:
@@ -58,6 +54,10 @@ class TestTaxi(unittest.TestCase):
                 taxi.speed += (-SPEED, SPEED)
             elif keys[pg.K_KP3]:
                 taxi.speed += (SPEED, SPEED)
+
+            # keep taxi off the cave walls
+            if pg.sprite.spritecollideany(taxi, scene, collided=pg.sprite.collide_mask):
+                taxi.speed.rotate_ip(180)
 
             player.update()
             taxi.rect.center = taxi.pos
