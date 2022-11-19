c8e5f932565d2258e165d5275f499afdf194f47c
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-11-19 13:25:46 +0100

    create scene with the player

---
+++ test/test_taxi.py
@@ -0,0 +1,72 @@
+"""Test facilities with a Space Taxi-like game."""
+
+
+from code.mysprite import MySprite
+import pygame as pg
+import unittest
+
+
+SPEED = 0.05
+FRICTION = (0.95, 0.95)
+GRAVITY = (0, 0.00005)
+
+
+
+
+
+class TestTaxi(unittest.TestCase):
+    """Close the window to end the tests."""
+
+    def setUp(self) -> None:
+        pg.init()
+        self.screen = pg.display.set_mode((320, 240), flags=pg.SCALED)
+        self.screen_rect = self.screen.get_rect()
+        pg.display.set_caption("Sprite Test")
+        self.clock = pg.time.Clock()
+    
+
+    def test_taxi_game(self):
+        scene_bgr = pg.image.load("test/taxi_scene.png")
+        scene_mask = pg.mask.from_surface(scene_bgr)
+
+        taxi = MySprite((300, 200), (0, 0), size=(16, 8), color="orange", gravity=GRAVITY, friction=FRICTION)
+        player = pg.sprite.GroupSingle(taxi)
+
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    return
+            
+            self.screen.fill("black")
+            self.screen.blit(scene_bgr, (0, 0))
+            
+            # handle key input
+            keys = pg.key.get_pressed()
+            if keys[pg.K_KP4]:
+                taxi.speed += (-SPEED, 0)
+            elif keys[pg.K_KP6]:
+                taxi.speed += (SPEED, 0)
+            elif keys[pg.K_KP8]:
+                taxi.speed += (0, -SPEED)
+            elif keys[pg.K_KP2]:
+                taxi.speed += (0, SPEED)
+            elif keys[pg.K_KP7]:
+                taxi.speed += (-SPEED, -SPEED)
+            elif keys[pg.K_KP9]:
+                taxi.speed += (SPEED, -SPEED)
+            elif keys[pg.K_KP1]:
+                taxi.speed += (-SPEED, SPEED)
+            elif keys[pg.K_KP3]:
+                taxi.speed += (SPEED, SPEED)
+
+            player.update()
+            taxi.rect.center = taxi.pos
+
+            player.draw(self.screen)
+
+            pg.display.flip()
+            self.clock.tick(60)
+
+
+    def tearDown(self) -> None:
+        pg.quit()
