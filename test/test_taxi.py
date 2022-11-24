3c3882a5fca41a619ea10cbc0851a79118f4262b
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-11-24 19:03:21 +0100

    outsource taxi

--- test/test_taxi.py
+++ test/test_taxi.py
@@ -4,11 +4,63 @@
 from code.mysprite import MySprite
 import pygame as pg
 import unittest
+import typing
 
 
 SPEED = 0.05
 FRICTION = (0.99, 0.99)
 GRAVITY = (0, 0.00005)
+
+
+class Taxi(MySprite):
+    def __init__(self, pos: tuple[int, int], speed: tuple[float, float], image_file: str = None, **kwargs: typing.Any) -> None:
+        super().__init__(pos, speed, image_file, **kwargs)
+        # although the taxi has only 3 lives (hulls), an original hull damage
+        # seems to occur, so if i want 3 hulls, i have to start with a 4
+        # perhaps it's because one sprite is inside another
+        self._hull = 4
+        self._passenger = pg.sprite.GroupSingle()
+
+    def collide(self):
+        self.speed.rotate_ip(180)
+        self._hull -= 1
+
+    def is_wrecked(self) -> bool:
+        return self._hull < 1
+
+    def get_input(self, keys):
+        if keys[pg.K_KP4]:
+            self.speed += (-SPEED, 0)
+        elif keys[pg.K_KP6]:
+            self.speed += (SPEED, 0)
+        elif keys[pg.K_KP8]:
+            self.speed += (0, -SPEED)
+        elif keys[pg.K_KP2]:
+            self.speed += (0, SPEED)
+        elif keys[pg.K_KP7]:
+            self.speed += (-SPEED, -SPEED)
+        elif keys[pg.K_KP9]:
+            self.speed += (SPEED, -SPEED)
+        elif keys[pg.K_KP1]:
+            self.speed += (-SPEED, SPEED)
+        elif keys[pg.K_KP3]:
+            self.speed += (SPEED, SPEED)
+
+    def update(self, *args, **kwargs):
+        super().update()
+        self.rect.center = self.pos
+
+    @property
+    def passenger(self) -> MySprite:
+        return self._passenger.sprite
+
+    def pickup(self, survivor:MySprite) -> None:
+        self._passenger.add(survivor)
+
+    def deliver(self) -> MySprite:
+        survivor = self._passenger33333333333333333.sprite
+        self._passenger.empty()
+        return survivor
 
 
 class TestTaxi(unittest.TestCase):
@@ -28,13 +80,8 @@ class TestTaxi(unittest.TestCase):
         rescue_area = pg.Rect(288, 192, 32, 32)
 
         # setup player
-        taxi = MySprite((300, 200), (0, 0), size=(16, 8), color="orange", gravity=GRAVITY, friction=FRICTION)
+        taxi = Taxi((300, 200), (0, 0), size=(16, 8), color="orange", gravity=GRAVITY, friction=FRICTION)
         player = pg.sprite.GroupSingle(taxi)
-        # although the taxi has only 3 lives (hulls), an original hull damage
-        # seems to occur, so if i want 3 hulls, i have to start with a 4
-        # perhaps it's because one sprite is inside another
-        hull = 4
-        occupied = pg.sprite.GroupSingle()
 
         # setup survivors
         survivors = pg.sprite.Group()
@@ -53,51 +100,31 @@ class TestTaxi(unittest.TestCase):
             scene.draw(self.screen)
 
             # handle key input
-            keys = pg.key.get_pressed()
-            if keys[pg.K_KP4]:
-                taxi.speed += (-SPEED, 0)
-            elif keys[pg.K_KP6]:
-                taxi.speed += (SPEED, 0)
-            elif keys[pg.K_KP8]:
-                taxi.speed += (0, -SPEED)
-            elif keys[pg.K_KP2]:
-                taxi.speed += (0, SPEED)
-            elif keys[pg.K_KP7]:
-                taxi.speed += (-SPEED, -SPEED)
-            elif keys[pg.K_KP9]:
-                taxi.speed += (SPEED, -SPEED)
-            elif keys[pg.K_KP1]:
-                taxi.speed += (-SPEED, SPEED)
-            elif keys[pg.K_KP3]:
-                taxi.speed += (SPEED, SPEED)
+            taxi.get_input(pg.key.get_pressed())
 
             # cave walls damages the taxi
             if pg.sprite.spritecollideany(taxi, scene, collided=pg.sprite.collide_mask):
-                taxi.speed.rotate_ip(180)
-                hull -= 1
-            
-            if hull < 1:
-                return
+                taxi.collide()
+                if taxi.is_wrecked():
+                    return
             
             player.update()
-            taxi.rect.center = taxi.pos
 
             for survivor in survivors:
                 survivor.rect.topleft = survivor.pos
             
             # if the taxi meets a survivor and it's vacant, pick him up
             survivor = pg.sprite.spritecollide(taxi, survivors, False)
-            if not occupied.sprite:
+            if not taxi.passenger:
                 survivors.remove(survivor)
-                occupied.add(survivor)
+                taxi.pickup(survivor)
             
             # check if the taxi is in the rescue area, and if it's occupied,
-            # unload the survivor
+            # deliver the survivor
             if rescue_area.contains(taxi.rect):
-                survivor = occupied.sprite
+                survivor = taxi.passenger
                 if survivor:
-                    occupied.remove(survivor)
-                    rescued.add(survivor)
+                    rescued.add(taxi.deliver())
                 # end game if all survivors rescued
                 if not survivors:
                     return
